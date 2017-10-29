9bb5b756851b049f12afe54d0bd59503b520cb85
Sam Cheng <samcheng8@gmail.com>
2017-10-28 18:25:31 -0600

    finish server tests

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -5,6 +5,12 @@ import SimpleSchema from 'simpl-schema'
 
 export const Notes = new Mongo.Collection('notes')
 
+if(Meteor.isServer) {
+  Meteor.publish('notes', function() {
+    return Notes.find({ userId: this.userId })
+  })
+}
+
 Meteor.methods({
   'notes.insert'() {
     if(!this.userId) {
@@ -31,6 +37,36 @@ Meteor.methods({
     }).validate({ _id })
 
     return Notes.remove({ _id, userId: this.userId })
+  },
+  'notes.update'(_id, updates) {
+    if(!this.userId) {
+      throw new Meteor.Error('not-authorized')
+    }
 
-  }
+    new SimpleSchema({
+      _id: {
+        type: String,
+        min: 1
+      },
+      title: {
+        type: String,
+        optional: true
+      },
+      body: {
+        type: String,
+        optional: true
+      }
+    }).validate({
+      _id,
+      ...updates
+     })
+
+     Notes.update({ _id, userId: this.userId }, { $set: {
+         updatedAt: moment().valueOf(),
+         ...updates
+       }
+     })
+
+
+   }
 })
--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -5,15 +5,25 @@ import { Notes } from './notes'
 
 if(Meteor.isServer) {
   describe('notes', function() {
+    const noteOne = {
+      _id: 'testId1',
+      title: 'title',
+      body: 'this is the body',
+      updatedAt: 0,
+      userId: 'test'
+    }
+
+    const noteTwo = {
+      _id: 'testId2',
+      title: 'another title',
+      body: 'this is another body',
+      updatedAt: 0,
+      userId: 'test2'
+    }
     beforeEach(function() {
       Notes.remove({})
-      Notes.insert({
-        _id: 'testId1',
-        title: 'title',
-        body: 'this is the body',
-        updatedAt: 0,
-        userId: 'test'
-      })
+      Notes.insert(noteOne)
+      Notes.insert(noteTwo)
     })
 
     it('should insert new note', function() {
@@ -24,9 +34,91 @@ if(Meteor.isServer) {
     })
 
     it('should remove note', function() {
-      Meteor.server.method_handlers['notes.remove'].apply({ userId: 'test'}, ['testId1'])
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId}, [noteOne._id])
 
-      chai.expect(Notes.findOne({_id: 'testId1'})).to.not.exist
+      chai.expect(Notes.findOne({_id: noteOne._id})).to.not.exist
     })
-  })
+
+    it('should not remove note if unauthenticated', function() {
+      chai.expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({}, [noteOne._id])
+      }).to.throw()
+    })
+
+    it('should not remove note if invalid id', function() {
+      chai.expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: noteOne.userId })
+      }).to.throw()
+    })
+
+    it('should update note', function() {
+        const title = "updated title"
+        Meteor.server.method_handlers['notes.update'].apply(
+          { userId: noteOne.userId },
+           [
+             noteOne._id,
+             { title }
+           ])
+
+           const note = Notes.findOne(noteOne._id)
+
+           chai.expect(note.updatedAt).to.be.above(0)
+           chai.expect(note).to.include({
+             title,
+             body: noteOne.body
+           })
+      })
+
+    it('should throw error if extra updates', function() {
+         chai.expect(() => {
+           Meteor.server.method_handlers['notes.update'].apply(
+             { userId: noteOne.userId },
+              [
+                noteOne._id,
+                { name: 'me' }
+              ])
+         }).to.throw()
+      })
+
+    it('should not update note if not note creator', function() {
+      const title = "updated title"
+      Meteor.server.method_handlers['notes.update'].apply(
+        { userId: 'kdkd' },
+         [
+           noteOne._id,
+           { title }
+         ])
+
+         const note = Notes.findOne(noteOne._id)
+
+         chai.expect(note).to.include(noteOne)
+    })
+
+    it('should not update note if unauthenticated', function() {
+      chai.expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({}, [noteOne._id])
+      }).to.throw()
+    })
+
+    it('should not update note if invalid id', function() {
+      chai.expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({ userId: noteOne.userId })
+      }).to.throw()
+    })
+
+    it('should return users notes', function() {
+      const result = Meteor.server.publish_handlers.notes.apply({ userId: noteOne.userId })
+      const notes = result.fetch()
+
+      chai.expect(notes.length).equal(1)
+      chai.expect(notes[0]).to.eql(noteOne)
+    })
+
+    it('should return users no notes if has none', function() {
+      const result = Meteor.server.publish_handlers.notes.apply({ userId: "not user" })
+      const notes = result.fetch()
+
+      chai.expect(notes.length).equal(0)
+    })
+})
 }
